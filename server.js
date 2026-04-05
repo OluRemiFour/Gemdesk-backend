a68d049544674d3ce90f804a9d5947d5a7a3e23d
RemmyAcee <remmydesigns38@gmail.com>
2026-04-05 14:19:55 +0100

    fixed email: now uses emailjs-web

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 // MongoDB Connection
